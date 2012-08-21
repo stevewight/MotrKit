088de790138d7a908d56a28af4bfc50582f6f582
Steve <stevendouglaswight@gmail.com>
2012-08-20 20:05:06 -0700

    Initial Commit

---
+++ Wight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wight
+//
+//  Created by Steve Wight on 8/20/12.
+//  Copyright (c) 2012 Ziato Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZLAppDelegate class]));
+    }
+}
